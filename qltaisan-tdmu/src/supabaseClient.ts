74005514ef5b89570a70f6c315f621c7d08c8e85
NguyenKhoi04 <ngkhoi04@gmail.com>
2026-04-12 20:30:50 +0700

    58

--- qltaisan-tdmu/src/supabaseClient.ts
+++ qltaisan-tdmu/src/supabaseClient.ts
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "../../database.types";
 
+
 // ✅ KHÔNG dùng dotenv
+
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
